cb423eb30cd2fc6038c0a216363e83e9694c738b
Nanoc <>
2018-08-03 17:21:30 -0700

    Automated commit at 2018-08-04 00:21:30 UTC by Nanoc 4.9.3

--- scripts/tutorials.js
+++
@@ -1,8 +0,0 @@
-var revealAnswers = function(parentId) {
-  var parent = document.getElementById(parentId);
-  var answers = parent.querySelectorAll('.answer');
-  for (var i = 0; i < answers.length; i++) {
-    answers[i].classList.remove('answer');
-    answers[i].classList.add('answered');
-  }
-};
